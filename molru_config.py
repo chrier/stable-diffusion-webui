d3516a02c437093863f1e9e53a157044925e22db
norhu1130 <norhu1130@naver.com>
2022-10-09 21:23:13 +0900

    설정 파일 설명 수정

--- molru_config.py
+++ molru_config.py
@@ -1,6 +1,6 @@
-# 몰루 Web UI 관련 설정
+# 몰루 Web UI 설정
 # 활성화 : True
-# 비활성회 : False
-# V2 활성화 여부(기본 : True)'
+# 비활성화 : False
+
 class Config(object):
-    v2_enable = True
+    v2_enable = True  # V2 활성화, 기본 : 활성화(True)
